696551946ce77bd7637c6aee0927229207c84bb4
yanzhifeng <402674280@qq.com>
2022-06-27 17:03:56 +0800

    init debug-react

---
+++ src/react/packages/scheduler/index.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+export * from './src/forks/Scheduler';
+
+// sy 添加以下
+export {
+  unstable_flushAllWithoutAsserting,
+  unstable_flushNumberOfYields,
+  unstable_flushExpired,
+  unstable_clearYields,
+  unstable_flushUntilNextPaint,
+  unstable_flushAll,
+  unstable_yieldValue,
+  unstable_advanceTime,
+  unstable_setDisableYieldValue,
+} from './src/forks/SchedulerMock';
